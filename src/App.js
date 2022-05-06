f4c4526f89030cfb93fce0aba732b5da1b376d77
sana-pro <sannaa0012@gmail.com>
2022-05-06 16:27:32 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import { useEffect, useState, useRef } from "react";
+import faceapi from './js/face-api.min';
 function App() {
+    const [modelLoaded, setModelLoaded] = useState(false);
+
+    const video = useRef(null);
+    useEffect(() => {
+        Promise.all([
+            faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+            faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+            faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+        ]).then(start)
+    }, [])
+    function start() {
+        setModelLoaded(true);
+        window.navigator.getUserMedia(
+            { video:{} },
+            stream => video.srcObject = stream,
+            err => console.error(err)
+        )
+
+        // video.src = '../videos/speech.mp4'
+        //for uploded video
+        console.log('video added')
+        recognizeFaces()
+    }
+    function loadLabeledImages() {
+        // const labels = ['Black Widow', 'Captain America', 'Hawkeye' , 'Jim Rhodes', 'Tony Stark', 'Thor', 'Captain Marvel']
+        const labels = ['Madiha','Hassan','Hayya','Sana'] // for WebCam
+        // const labels = ['Hassan' ] // for WebCam
+        return Promise.all(
+            labels.map(async (label)=>{
+                const descriptions = []
+                for(let i=1; i<=5; i++) {
+                    const img = await faceapi.fetchImage(`../public/labeled_images/${label}/${i}.jpg`)
+                    const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                    console.log(label + i + JSON.stringify(detections))
+                    descriptions.push(detections.descriptor)
+                }
+                document.body.append(label+' Faces Loaded | ')
+                return new faceapi.LabeledFaceDescriptors(label, descriptions)
+            })
+        )
+    }
+    async function recognizeFaces() {
+
+        const labeledDescriptors = await loadLabeledImages()
+        console.log(labeledDescriptors)
+        const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7)
+
+
+        video.addEventListener('play', async () => {
+            console.log('Playing')
+            const canvas = faceapi.createCanvasFromMedia(video)
+            document.body.append(canvas)
+
+            const displaySize = { width: video.width, height: video.height }
+            faceapi.matchDimensions(canvas, displaySize)
+
+
+
+            setInterval(async () => {
+                const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+
+                const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+
+                const results = resizedDetections.map((d) => {
+                    return faceMatcher.findBestMatch(d.descriptor)
+                })
+                results.forEach( (result, i) => {
+                    const box = resizedDetections[i].detection.box
+                    console.log(result.toString())
+                    // code for matching img and rfid
+                    // if(result.toString().includes("Hassan")){ //username from database
+                    //  document.getElementById("#errST").innerHTML = "errrrooorrrrrrrr";
+                    // }
+                    const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+                    drawBox.draw(canvas)
+                })
+            }, 100)
+
+
+
+        })
+    }
   return (
     <div className="App">
 
         <h1 id="errST">sana</h1>
-        <video id="videoInput" width="720" height="550" muted controls/>
+        <video ref={video} id="videoInput" width="720" height="550" muted controls/>
     </div>
   );
 }
